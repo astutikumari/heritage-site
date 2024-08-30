a19eb514a11a764d000792f784338f33e8cec470
Astuti kumari <142251274+astutikumari@users.noreply.github.com>
2024-08-30 20:25:45 +0530

    Delete festival.js

--- festival.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll('.festival-card');
-
-    cards.forEach(card => {
-        card.addEventListener('click', function() {
-            card.classList.toggle('expanded');
-        });
-    });
-});
